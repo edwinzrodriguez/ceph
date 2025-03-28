5a0a8741d0794b7d21d3a68fb8ee9396304ab416
Anoop C S <anoopcs@cryptolab.net>
2025-03-28 08:00:32 +0530

    libcephfs: Implement ceph_ll_get() API
    ceph_ll_get() updates the reference to the corresponding inode.
    
    Signed-off-by: Anoop C S <anoopcs@cryptolab.net>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -13476,6 +13476,12 @@ bool Client::_ll_forget(Inode *in, uint64_t count)
   return last;
 }
 
+void Client::ll_get(Inode *in)
+{
+  std::scoped_lock lock(client_lock);
+  _ll_get(in);
+}
+
 bool Client::ll_forget(Inode *in, uint64_t count)
 {
   std::scoped_lock lock(client_lock);
