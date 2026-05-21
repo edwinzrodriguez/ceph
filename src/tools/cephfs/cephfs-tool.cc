7b879c8d9d4962290cf1543236b329183e0b2979
Edwin Rodriguez <edwin.rodriguez1@ibm.com>
2026-05-21 09:21:32 -0400

    cephfs-tool: set thread names for read/write workers
    Signed-off-by: Edwin Rodriguez <edwin.rodriguez1@ibm.com>

--- src/tools/cephfs/cephfs-tool.cc
+++ src/tools/cephfs/cephfs-tool.cc
@@ -402,6 +402,7 @@ bench_write_worker(
   struct ceph_mount_info *cmount = shared_cmount;
   auto duration_limit = std::chrono::seconds(config.duration);
 
+  ceph_pthread_setname(("wr-worker-" + std::to_string(thread_id)).c_str());
   if (config.per_thread_mount) {
     if (int rc = setup_mount(&cmount, config, ss); rc < 0) {
       ss << "Thread " << thread_id << " mount failed: " << strerror(-rc) << std::endl;
@@ -715,6 +716,7 @@ bench_read_worker(
 
   struct ceph_mount_info *cmount = shared_cmount;
   auto duration_limit = std::chrono::seconds(config.duration);
+  ceph_pthread_setname(("rd-worker-" + std::to_string(thread_id)).c_str());
 
   if (config.per_thread_mount) {
     if (int rc = setup_mount(&cmount, config, ss); rc < 0) {
