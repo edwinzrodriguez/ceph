1548cc8b17302094acf4f230b6d632cbdc0e6ce9
Edwin Rodriguez <edwin.rodriguez1@ibm.com>
2026-07-01 08:44:29 -0400

    client: fix async read lock ordering and cap lifecycle
    Harden async read and readahead cap/file-handle lifecycle.
    
    Release FILE_RD cap refs on the async read zero-length/EOF fast path.
    Pin the inode in C_Readahead and defer all fh/inode work to client_finisher
    so abort paths do not race with cap teardown.
    
    Signed-off-by: Edwin Rodriguez <edwin.rodriguez1@ibm.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -10416,6 +10416,11 @@ success:
 
 error:
 
+  if ((have_caps & CEPH_CAP_FILE_RD) &&
+      in->cap_refs[CEPH_CAP_FILE_RD] > 0) {
+    clnt->put_cap_ref(in, CEPH_CAP_FILE_RD);
+  }
+
   onfinish->complete(r);
   fini = true;
 }
@@ -10642,23 +10647,29 @@ done:
   return rc;
 }
 
-Client::C_Readahead::C_Readahead(Client *c, Fh *f) :
-    client(c), f(f), start_time(ceph_clock_now()) {
+Client::C_Readahead::C_Readahead(Client *c, Fh *f, Inode *inode) :
+    client(c), f(f), in(inode), start_time(ceph_clock_now()) {
   f->get();
   f->readahead.inc_pending();
 }
 
 Client::C_Readahead::~C_Readahead() {
-  f->readahead.dec_pending();
-  client->_put_fh(f);
+  // finish() hands off fh cleanup to client_finisher; only handle the
+  // synchronous-abort path (delete without finish) here.
+  if (f) {
+    f->readahead.dec_pending();
+    client->_put_fh(f);
+  }
 }
 
 void Client::C_Readahead::finish(int r) {
   // ObjectCacher may call this with its cache lock held (e.g. writex waking
-  // waitfor_read).  Defer client_lock work to client_finisher.
-  InodeRef in = f->inode;
+  // waitfor_read).  Defer all fh/inode work to client_finisher under
+  // client_lock.  Pin the inode in the ctor; do not touch f->inode here.
+  Fh *fh = f;
+  f = nullptr;
   client->client_finisher.queue(new LambdaContext(
-    [client=client, in=std::move(in), r](int) {
+    [client=client, in=in, fh, r](int) {
       ClientLockIfNeeded lock(client);
       lgeneric_subdout(client->cct, client, 20) << "client." << client->get_nodeid() << " C_Readahead on " << in << dendl;
       if (in->cap_refs[CEPH_CAP_FILE_RD] > 0) {
@@ -10670,6 +10681,8 @@ void Client::C_Readahead::finish(int r) {
       if (r > 0) {
 	      client->update_read_io_size(r);
       }
+      fh->readahead.dec_pending();
+      client->_put_fh(fh);
     }));
 }
 
@@ -10681,7 +10694,7 @@ void Client::do_readahead(Fh *f, Inode *in, uint64_t off, uint64_t len)
     if (readahead_extent.second > 0) {
       ldout(cct, 20) << "readahead " << readahead_extent.first << "~" << readahead_extent.second
 		     << " (caller wants " << off << "~" << len << ")" << dendl;
-      Context *onfinish2 = new C_Readahead(this, f);
+      Context *onfinish2 = new C_Readahead(this, f, in);
       InodeOCState oc(in);
       int r2 = 0;
       {
